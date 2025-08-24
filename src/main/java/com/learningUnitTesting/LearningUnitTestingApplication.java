7ea5213895f4cb05dc3ad2bb6bd7f3e1a5d48449
Mayur <mayurvalte1999@gmail.com>
2025-08-24 17:46:53 +0530

    Initial Commit

---
+++ src/main/java/com/learningUnitTesting/LearningUnitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.learningUnitTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningUnitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningUnitTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningUnitTesting/LearningUnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningUnitTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningUnitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
