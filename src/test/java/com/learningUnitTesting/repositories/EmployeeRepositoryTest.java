c27532c143b689d98185116d9ed32438e2969168
Mayur <mayurvalte1999@gmail.com>
2025-08-25 23:17:01 +0530

    Basics of Mockito

--- src/test/java/com/learningUnitTesting/repositories/EmployeeRepositoryTest.java
+++ src/test/java/com/learningUnitTesting/repositories/EmployeeRepositoryTest.java
@@ -1,60 +1,91 @@
 package com.learningUnitTesting.repositories;
 
 import com.learningUnitTesting.TestContainerConfiguration;
+import com.learningUnitTesting.dto.EmployeeDto;
 import com.learningUnitTesting.entities.Employee;
+import com.learningUnitTesting.services.impl.EmployeeServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
-@Import(TestContainerConfiguration.class)
-@DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class EmployeeRepositoryTest {
+@Import(TestContainerConfiguration.class)
+@ExtendWith(MockitoExtension.class)
+class EmployeeServiceImplTest {
 
-    @Autowired
+    @Mock
     private EmployeeRepository employeeRepository;
 
-    private Employee employee;
+    @Spy
+    private ModelMapper modelMapper;
+
+    @InjectMocks
+    private EmployeeServiceImpl employeeService;
+
+    private Employee mockEmployee;
+    private EmployeeDto mockEmployeeDto;
 
     @BeforeEach
     void setUp() {
-        employee = Employee.builder()
-                .name("Anuj")
+        mockEmployee = Employee.builder()
+                .id(1L)
                 .email("anuj@gmail.com")
-                .salary(100L)
+                .name("Anuj")
+                .salary(200L)
                 .build();
+
+        mockEmployeeDto = modelMapper.map(mockEmployee, EmployeeDto.class);
     }
 
     @Test
-    void testFindByEmail_whenEmailIsPresent_thenReturnEmployee() {
-//        Arrange, Given
-        employeeRepository.save(employee);
+    void testGetEmployeeById_WhenEmployeeIdIsPresent_ThenReturnEmployeeDto() {
+//        assign
+        Long id = mockEmployee.getId();
+        when(employeeRepository.findById(id)).thenReturn(Optional.of(mockEmployee)); //stubbing
 
-//        Act, When
-        List<Employee> employeeList = employeeRepository.findByEmail(employee.getEmail());
+//        act
+        EmployeeDto employeeDto = employeeService.getEmployeeById(id);
 
-//        Assert, Then
-        assertThat(employeeList).isNotNull();
-        assertThat(employeeList).isNotEmpty();
-        assertThat(employeeList.get(0).getEmail()).isEqualTo(employee.getEmail());
+//        assert
+        assertThat(employeeDto).isNotNull();
+        assertThat(employeeDto.getId()).isEqualTo(id);
+        assertThat(employeeDto.getEmail()).isEqualTo(mockEmployee.getEmail());
+        verify(employeeRepository, only()).findById(id);
     }
 
     @Test
-    void testFindByEmail_whenEmailIsNotFound_thenReturnEmptyEmployeeList() {
-//        Given
-        String email = "notPresent.123@gmail.com";
-//        When
-        List<Employee> employeeList = employeeRepository.findByEmail(email);
-//        Then
-        assertThat(employeeList).isNotNull();
-        assertThat(employeeList).isEmpty();
+    void testCreateNewEmployee_WhenValidEmployee_ThenCreateNewEmployee() {
+//        assign
+        when(employeeRepository.findByEmail(anyString())).thenReturn(List.of());
+        when(employeeRepository.save(any(Employee.class))).thenReturn(mockEmployee);
+//        act
+
+        EmployeeDto employeeDto = employeeService.createNewEmployee(mockEmployeeDto);
+//        assert
+        assertThat(employeeDto).isNotNull();
+        assertThat(employeeDto.getEmail()).isEqualTo(mockEmployeeDto.getEmail());
+
+        ArgumentCaptor<Employee> employeeArgumentCaptor = ArgumentCaptor.forClass(Employee.class);
+        verify(employeeRepository).save(employeeArgumentCaptor.capture());
+
+        Employee capturedEmployee = employeeArgumentCaptor.getValue();
+        assertThat(capturedEmployee.getEmail()).isEqualTo(mockEmployee.getEmail());
     }
+
+
+
 }
-
